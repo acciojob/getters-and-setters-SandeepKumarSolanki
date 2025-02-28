b761e64e5373ef1228b51fcafc25a1058e115d59
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-01 00:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -41,6 +41,8 @@ console.log(person.name);  // Output: John
 
 person.age = 30;  // Using the setter to change the age
 console.log(person.age);  // Output: 30
+person.setAge(30);
+console.log(perosn.getAge())
 
 // Creating a Student instance
 const student = new Student("Alice", 22);
