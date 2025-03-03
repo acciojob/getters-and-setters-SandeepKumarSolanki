54e641d4bd018244274c1e4be9413fc7bfa13d99
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-03 21:03:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,27 +25,11 @@ class Student extends Person {
 }
 
 class Teacher extends Person {
-
-  teach() {
-    console.log(`${this.personName} is teaching`);
-  }
+	teach() {
+	    console.log(`${this.personName} is teaching`);
+	  }
 }
 
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
-
-person.setAge(30);  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-console.log(person.name); // Output: John (name remains unchanged)
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
